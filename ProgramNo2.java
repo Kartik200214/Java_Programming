e728817918e2299322466103d9fd6302124d36bc
Kartik -KK <109861395+Kartik200214@users.noreply.github.com>
2022-12-16 23:20:06 +0530

    Add files via upload

---
+++ ProgramNo2.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Arithematic
+{
+    public int iValue1; 
+    public int iValue2;
+
+    public Arithematic(int i,int j)
+    {
+        iValue1 = i;
+        iValue2 = j;
+    }
+
+
+    public int Addition(int i,int j)
+    {
+        int Sum = 0;
+        Sum = iValue1 + iValue2;
+        return Sum;
+    }
+}
+
+class ProgramNo2
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        
+        int iNo1 = 0 ,iNo2 = 0,iAns = 0;
+
+        System.out.println("Enter first Number: ");
+        iNo1 = sobj.nextInt();
+
+        System.out.println("Enter Second Number: ");
+        iNo2 = sobj.nextInt();
+
+        Arithematic aobj = new Arithematic(iNo1,iNo2);
+
+        iAns = aobj.Addition(iNo1,iNo2);
+
+        System.out.println("Addition is : "+iAns);
+
+    }
+}
