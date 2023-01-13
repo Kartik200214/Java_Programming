d3f04add9244fffe4a1f7d993e6c363ad10e9715
Kartik -KK <109861395+Kartik200214@users.noreply.github.com>
2023-01-13 23:20:03 +0530

    Create Loop1.java

---
+++ Basic Loop/Loop1.java
@@ -0,0 +1,15 @@
+public class Loop1 
+{
+    public static void main(String args[])
+    {
+        int counter = 0;
+        while(counter < 10)
+        {
+            System.out.println("Hello World "+counter);
+            counter++;
+        }
+        System.out.println("Printer HW 100 times");
+
+    }
+    
+}
